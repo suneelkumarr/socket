ec242e73526e06c7835fba216a42c89d2b201ad8
Suneel Kumar <sunilkumar05644@gmail.com>
2021-09-15 16:09:54 +0530

    database connected

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
+require("dotenv").config()
 const express = require('express');
 const cors = require('cors')
 const morgan = require("morgan");
+const mongoose = require('mongoose');
 const app = express();
 const server = require('http').createServer(app)
 const port = process.env.PORT || 4000;
@@ -10,22 +12,34 @@ const io = require("socket.io")(server,{
         methods:["GET","POST"]
     },
 });
+
+//view js connected
 app.set('views', './views')
 app.set('view engine', 'ejs')
 app.use(express.static('public'))
 app.use(express.urlencoded({ extended: true }))
+
+//middleware
 app.use(cors());
 app.use(express.json());
-app.use(cors())
 app.use(morgan("common"));
 
 let rooms = {}
-const typers = {}
 
+
+//database conneted
+mongoose.connect(process.env.MONGO_URL, { useNewUrlParser: true, useUnifiedTopology: true }).then(() => {
+  console.log('database is connected')
+}).catch(err => console.log(err))
+
+
+//indexfile
 app.get('/',(req,res)=>{ 
     res.render('index', { rooms: rooms })
 });
 
+
+//post request
 app.post('/room', (req, res) => {
     if (rooms[req.body.room] != null) {
       return res.redirect('/')
@@ -36,7 +50,7 @@ app.post('/room', (req, res) => {
     io.emit('room-created', req.body.room)
   });
 
-
+//get request params
 app.get('/:room', (req, res) => {
     if (rooms[req.params.room] == null) {
       return res.redirect('/')
@@ -44,14 +58,17 @@ app.get('/:room', (req, res) => {
     res.render('room', { roomName: req.params.room })
   })
 
-
+//socket connect
 io.on('connect', (socket) => {
     console.log('user is connected');
+    //user is connected
     socket.on('new-user', (room, name) => {
         socket.join(room)
         rooms[room].users[socket.id] = name
         socket.to(room).emit('user-connected', name)
       })
+
+      //send the message to the user
       socket.on('send-chat-message', (room, message) => {
         socket.to(room).emit('chat-message', { message: message, name: rooms[room].users[socket.id] })
       })
@@ -83,4 +100,5 @@ function getUserRooms(socket) {
 
 server.listen(port,() => {
     console.log(`Server is running on ${port} `);
+    console.log(`http://localhost:${port}`);
 });
